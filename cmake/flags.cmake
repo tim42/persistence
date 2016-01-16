55af1abef3e168e3212f768270988e2c3b3e5e36
tim42 <timothee.feuillet@gmail.com>
2016-01-16 15:55:08 +0100

    [PERSISTENCE/CMAKE]: Update GCC debug flags by adding the -Og flag

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -18,7 +18,7 @@ if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
   set(PROJ_FLAGS "${PROJ_FLAGS} -std=gnu++14 -Wall -Wextra -Wno-unused-function")
 
   if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(PROJ_FLAGS "${PROJ_FLAGS} -fno-inline")
+    set(PROJ_FLAGS "${PROJ_FLAGS} -Og -fno-inline")
   else(CMAKE_BUILD_TYPE STREQUAL "Release")
     set(PROJ_FLAGS "${PROJ_FLAGS} -O3 -finline-limit=50000 -fwhole-program -fmerge-constants -fmerge-all-constants")
   else(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
